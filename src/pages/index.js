cfebbac40d455192d289104d971996923632dbc6
rezki bimo <rezkibimo@users.noreply.github.com>
2023-03-16 19:20:21 +1100

    initial works on the card section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,43 @@
 import * as React from "react"
+import "../style/style.scss"
+import ImageTest from "../images/lc-greater-collab@2x.jpg"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+
+
+    <section class="layout">
+      <div>
+        <div class="card">
+
+          <div class="container">
+            <img src={ImageTest} alt="Logo" />
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card">
+
+          <div class="container">
+            <img src={ImageTest} alt="Logo" />
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card">
+          <img src={ImageTest} alt="Logo" />
+          <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+  </div>
 }
