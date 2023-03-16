cbf5ebdb0562f83fb782b208d1293ab3c4a37e3c
rezki bimo <rezkibimo@users.noreply.github.com>
2023-03-16 20:02:01 +1100

    added responsive image resolution support
    automatically use 2x or 1x image based on device viewport width

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,38 +5,45 @@ import ImageTest from "../images/lc-greater-collab@2x.jpg"
 export default function Home() {
   return <div>
 
+    <div>
 
-    <section class="layout">
-      <div>
+      <h2>Card</h2>
+
+      <section class="layout">
+
         <div class="card">
-
+          <img 
+          src={ImageTest} 
+          srcset=
+  "{ImageTest} 1x, {ImageTest} 2x"
+          alt="Logo" />
           <div class="container">
-            <img src={ImageTest} alt="Logo" />
-            <h4><b>John Doe</b></h4>
+            <h4><b>Safe Documents</b></h4>
             <p>Architect & Engineer</p>
           </div>
         </div>
-      </div>
-      <div>
-        <div class="card">
 
-          <div class="container">
-            <img src={ImageTest} alt="Logo" />
-            <h4><b>John Doe</b></h4>
-            <p>Architect & Engineer</p>
-          </div>
-        </div>
-      </div>
-      <div>
         <div class="card">
           <img src={ImageTest} alt="Logo" />
           <div class="container">
-            <h4><b>John Doe</b></h4>
+            <h4><b>Work remotely</b></h4>
             <p>Architect & Engineer</p>
           </div>
         </div>
-      </div>
-    </section>
+
+        <div class="card">
+          <img src={ImageTest} alt="Logo" />
+          <div class="container">
+            <h4><b>Greater Collaboration</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+
+
+
+      </section>
+
+    </div>
 
 
   </div>
