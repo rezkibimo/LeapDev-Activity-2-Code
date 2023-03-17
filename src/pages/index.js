d5b0b3e139315d04deac0b8442f6468f231d9a1c
rezki bimo <rezkibimo@users.noreply.github.com>
2023-03-17 18:28:36 +1100

    styling fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,31 +8,33 @@ import IndexJumbotron from "../components/indexJumbotron"
 export default function Home() {
   return <div>
 
-    <nav class="menu">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
+    <div className="headerSize">
+      <nav class="menu">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
 
-    <header class="twoColoumnLayoutHeader">
+      <header class="twoColoumnLayoutHeader">
 
-      <div className="headerTextBox">
-        <h1 className="sectionTopHeader">The place to work with your lawyer</h1>
-        <p>Your lawyer uses LawConnect to share information with you about your legal case in a privite, convenient and secure way.</p>
-        <button>Create Free Account</button>
-      </div>
+        <div className="headerTextBox">
+          <h1 className="sectionTopHeader">The place to work with your lawyer</h1>
+          <p>Your lawyer uses LawConnect to share information with you about your legal case in a privite, convenient and secure way.</p>
+          <button>Create Free Account</button>
+        </div>
 
-      <div>
-        <StaticImage
-          src="../images/lc-hero-desktop@2x.png"
-          alt="Safe Documents"
-          width="611"
-        />
-      </div>
+        <div>
+          <StaticImage
+            src="../images/lc-hero-desktop@2x.png"
+            alt="Safe Documents"
+            width="611"
+          />
+        </div>
 
-    </header>
+      </header>
+    </div>
 
     <CardSection />
 
