7796b0077b48f97abd2e046bf362ad87d69aeb7a
rezki bimo <rezkibimo@users.noreply.github.com>
2023-03-17 16:44:24 +1100

    added header, footer and 2 section

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,45 +1,56 @@
 import React, { Component } from 'react'
 import { StaticImage } from "gatsby-plugin-image"
-import "../style/CardSection.scss"
+import "../style/cardSection.scss"
 
 export class CardSection extends Component {
     render() {
         return (
-            <div>
+            <div className='sectionContainer'>
 
                 <div>
-                    <h2 className='SectionHeader'>One Solution Designed to Make Your Firm More Money</h2>
+                    <h1 className='sectionHeader'>One Solution Designed to Make Your Firm More Money</h1>
+                    <br></br>
                 </div>
 
-                <section class="card-layout">
-                    <div class="card">
-                        <StaticImage
-                            src="../images/lc-safe-docs@2x.jpg"
-                            alt="Safe Documents"
-                        />
-                        <div class="card-body">
+                <section className="card-layout">
+                    <div className="card">
+                        <div className='card-image'>
+                            <StaticImage
+                                src="../images/lc-safe-docs@2x.jpg"
+                                alt="Safe Documents"
+                                // width="160px"
+                                maxHeight="160px"
+                                
+                                //layout='fullWidth'
+                                // imgStyle={{ height: '160px', width: '322px' }}
+                            />
+                        </div>
+
+                        <div className="card-body">
                             <h4>Safe Documents</h4>
                             <p>LawConnect stores its data on servers provided by Amazon Web Services and is safer than email.</p>
                         </div>
                     </div>
 
-                    <div class="card">
+                    <div className="card">
                         <StaticImage
                             src="../images/lc-work-remote@2x.jpg"
                             alt="Work remotely"
+                            layout="fullWidth"
                         />
-                        <div class="card-body">
+                        <div className="card-body">
                             <h4>Work remotely</h4>
                             <p>You can sign in to LawConnect and view, collaborate and share your documents from anywhere..</p>
                         </div>
                     </div>
 
-                    <div class="card">
+                    <div className="card">
                         <StaticImage
                             src="../images/lc-greater-collab@2x.jpg"
                             alt="Greater Collaboration"
+                            layout="fullWidth"
                         />
-                        <div class="card-body">
+                        <div className="card-body">
                             <h4>Greater Collaboration</h4>
                             <p>Collaborate on documents with clients, lawyers and other parties with LawConnect’s Comment and Reply.</p>
                         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,74 @@
 import * as React from "react"
-import "../style/style.scss"
 import { StaticImage } from "gatsby-plugin-image"
-import CardSection from "../components/CardSection"
+
+import "../style/index.scss"
+import CardSection from "../components/cardSection"
 
 export default function Home() {
   return <div>
 
-<CardSection />
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
 
+    <header class="twoColoumnLayout">
+
+      <div className="maxPW">
+        <h1 className="sectionHeader">The place to work with your lawyer</h1>
+        <p>Your lawyer uses LawConnect to share information with you about your legal case in a privite, convenient and secure way.</p>
+        <button>Create Free Account</button>
+      </div>
+
+      <div>
+        <StaticImage
+          src="../images/lc-hero-desktop@2x.png"
+          alt="Safe Documents"
+          width="611"
+        />
+      </div>
+
+    </header>
+
+    <CardSection />
+
+    <section class="twoColoumnLayout sectionContainer firstColor">
+
+      <div className="maxPW">
+        <h1 className="sectionHeader">Signing Documents Electronically</h1>
+        <p>Your lawyer will share documents related to your case with you, via LawConnect. Each time a document is shared by your lawyer, you will receive an email notification so you can access the document. If you already have a LawConnect account, you can login and an alert will be in your notifications tab that a document has been shared.</p>
+      </div>
+
+      <div>
+        <StaticImage
+          src="../images/lc-share-docs@2x.png"
+          alt="Safe Documents"
+          width="372"
+        />
+      </div>
+
+    </section>
+
+    <section class="twoColoumnLayout sectionContainer">
+      <div>
+        <StaticImage
+          src="../images/lc-sign-docs-desktop@2x.png"
+          alt="Safe Documents"
+          width="372"
+        />
+      </div>
+      <div className="maxPW">
+        <h1 className="sectionHeader">Signing Documents Electronically</h1>
+        <p>LawConnect integrates with DocuSign to provide our users with a quick, easy and secure way to sign importnt legal documents online.</p>
+      </div>
+    </section>
+
+    <footer>
+
+    </footer>
 
   </div>
 }
